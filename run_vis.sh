cf76f4590fbe91edcc20f6addfaed7e98edc7d1e
KexinotKevin <ccwang10027@gmail.com>
2026-03-31 00:22:06 +0800

    可解释性热图运行成功

--- run_vis.sh
+++ run_vis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PYTHONPATH="/home/shulab/anaconda3/envs/gnn_gpu/bin/python"
-$PYTHONPATH compute_graphs.py \
-    --combo_dir results/20260328_165042/S1200/atlas_bna246__sc_FA-fiber_count__fc_pcc_rest/split_70_15_15/seed_42/ \
+conda activate gnn_work2
+python compute_graphs.py \
+    --combo_dir results/20260330_154419/S1200/atlas_bna246__sc_FA-fiber_count__fc_pcc_rest/split_70_15_15/seed_42/ \
     --atlas bna246\
-    --plots_dir plots/20260328_165042/S1200/atlas_bna246__sc_FA-fiber_count__fc_pcc_rest/split_70_15_15/seed_42/\
+    --plots_dir plots/20260330_154419/S1200/atlas_bna246__sc_FA-fiber_count__fc_pcc_rest/split_70_15_15/seed_42/\
     --label "CogFluidComp_Unadj"
